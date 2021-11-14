6dfb74f9da7d3b1cb9e46762813609fc371088e1
Oscar marina <oscar7600@outlook.com>
2021-11-14 11:11:11 +0100

    feat: custom file extension

--- bin/index.js
+++ bin/index.js
@@ -10,11 +10,12 @@ const SassStyleTemplate = require('../lib/index.js');
 const customTemplate = path.resolve('.sass-template.tmpl');
 
 program
-  .version(pkg.version, '-v, --version','show version number')
+  .version(pkg.version, '-v, --version', 'show version number')
   .option('-s, --marker-start <string>', 'start replace position')
   .option('-e, --marker-end <string>', 'end replace position')
   .option('-g, --custom-glob <string>', 'string pattern to be matched')
   .option('-f, --css-file', 'generate css file instead of using template')
+  .option('-j, --js-file <string>', 'file extension')
   .option('-d, --destination <string>', 'location of the output file');
 
 program.parse(process.argv);
