536a60c2db6372c394b6cb32e20b43facb68b6ca
oscar marina <oscar7600@outlook.com>
2024-05-16 21:47:19 +0200

    chore: bump dependencies

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const program = require('commander');
+const { program } = require('commander');
 const path = require('path');
 const pkg = require('../package.json');
 
